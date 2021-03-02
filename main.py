f76112213ed9ab3b275d0beafbbf6aa47f31485f
D00dleman <vbif1099@gmail.com>
2021-03-02 16:09:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,46 +2,46 @@ from bottle import route, run, template, redirect
 import subprocess
 import json
 
-"""
-x = subprocess.Popen(["lsof", "-i:" + port], stdout=subprocess.PIPE).stdout.read()[:-1].decode("utf-8").split("\n")
 
-"""
-
-configFile = "config.json"
-ports = json.load(open(configFile, 'r'))
+config_file = "config.json"
+ports = json.load(open(config_file, 'r'))
 ports = [str(i) for i in ports]
-def lsof(port):
-  print("port")
-  return subprocess.Popen(["lsof", "-i:" + str(port)], stdout=subprocess.PIPE).stdout.read()[:-1].decode("utf-8")
 
+
+def lsof(port):
+    print("port")
+    return subprocess.Popen(
+        ["lsof", "-i:" + str(port)], 
+        stdout=subprocess.PIPE
+    ).stdout.read()[:-1].decode("utf-8")
+
+  
 def rpp(port): #read pid for port
-  x = lsof(port).split("\n")
-  retList = [i.split()[1] for i in x[1:]]
-  return retList
+    x = lsof(port).split("\n")
+    return [i.split()[1] for i in x[1:]]
+
 
 def killall(port):
-  subprocess.Popen(["kill", "-TERM"] + rpp(port))
-  if(rpp(port) == []):
-    return True
-  else:
-    return False
+    subprocess.Popen(["kill", "-TERM"] + rpp(port))
+    return rpp(port) == []
+  
 
 @route("/")
 def main():
-  global ports
-  lsofs = {}
-  prts = []
-  for i in ports:
-    lsofs[i] = lsof(i).split("\n")
-    prts.append(i)
-    #ret += "<p>" + "<br>".join(lsof(i).split("\n")) + "</p><br>"
-    #ret += '<a href="killall/' + i +'">Перезаргузить порт ' + i + '</a>'
-    #ret += "<hr>"
-  return template("index.tpl", lsofs=lsofs, ports=prts)
+    global ports
+    lsof_dict = {}
+    ports_list = []
+    for i in ports:
+        lsof_dict[i] = lsof(i).split("\n")
+        ports_list.append(i)
+    return template("index.tpl", lsofs=lsof_dict, ports=ports_list)
+
 
 @route("/killall/<port>")
 def ka(port):
-  killall(port)
-  redirect("/")
-  return str(port) + "<br>" + str(rpp(port)) + "<br>" + str(lsof(port))
+    killall(port)
+    redirect("/")
+    return "{0}<br>{1}<br>{2}".format(str(port), str(rpp(port)), str(lsof(port))
+
+  
 run(host="localhost", port="8080")
